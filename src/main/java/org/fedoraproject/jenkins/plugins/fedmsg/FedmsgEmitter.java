a5db5b054fa719086ba7c3989e0dde9f7a8b5b5a
Ricky Elrod <ricky@elrod.me>
2014-03-31 20:51:33 -0400

    This compiles, but is not tested yet

--- src/main/java/org/fedoraproject/jenkins/plugins/fedmsg/FedmsgEmitter.java
+++ src/main/java/org/fedoraproject/jenkins/plugins/fedmsg/FedmsgEmitter.java
@@ -5,8 +5,11 @@ import hudson.util.FormValidation;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.model.AbstractProject;
-import hudson.tasks.Builder;
+import hudson.model.Result;
+import hudson.tasks.Notifier;
 import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Publisher;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -14,72 +17,74 @@ import org.kohsuke.stapler.QueryParameter;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.io.File;
+import java.util.HashMap;
+
+import org.fedoraproject.fedmsg.*;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
 
 /**
- * Sample {@link Builder}.
- *
- * <p>
- * When the user configures the project and enables this builder,
- * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
- * and a new {@link FedmsgEmitter} is created. The created
- * instance is persisted to the project configuration XML by using
- * XStream, so this allows you to use instance fields (like {@link #name})
- * to remember the configuration.
- *
- * <p>
- * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)}
- * method will be invoked. 
+ * Send a message to the Fedmsg bus when a build is completed.
  *
  * @author Ricky Elrod
  */
-public class FedmsgEmitter extends Builder {
+public class FedmsgEmitter extends Notifier {
 
-    private final String name;
-
-    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
     @DataBoundConstructor
-    public FedmsgEmitter(String name) {
-        this.name = name;
-    }
-
-    /**
-     * We'll use this from the <tt>config.jelly</tt>.
-     */
-    public String getName() {
-        return name;
-    }
+    public FedmsgEmitter() { }
 
     @Override
-    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
-        // This is where you 'build' the project.
-        // Since this is a dummy, we just say 'hello world' and call that a build.
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+        String endpoint = getDescriptor().getUseStaging() ?
+            "tcp://hub.fedoraproject.org:9940"
+            : "tcp://hub.stg.fedoraproject.org:9940";
 
-        // This also shows how you can consult the global configuration of the builder
-        if (getDescriptor().getUseStaging())
-            listener.getLogger().println("Bonjour, "+name+"!");
-        else
-            listener.getLogger().println("Hello, "+name+"!");
+        String environment = getDescriptor().getUseStaging() ? "stg" : "prod";
+
+        FedmsgConnection fedmsg = new FedmsgConnection()
+            .setEndpoint(endpoint)
+            .setLinger(2000);
+
+        Result buildResult = build.getResult();
+        if (buildResult != null) {
+            HashMap<String, Object> message = new HashMap();
+            message.put("project", build.getProject().getName());
+            message.put("build", build.getNumber());
+
+            FedmsgMessage blob = new FedmsgMessage()
+                .setTopic("org.fedoraproject." + environment + ".jenkins.build." + buildResult.toString())
+                .setI(1)
+                .setTimestamp(new java.util.Date())
+                .setMessage(message);
+
+            try {
+                SignedFedmsgMessage signed = blob.sign(
+                    new File("/etc/pki/fedmsg/jenkins-jenkins.cloud.fedoraproject.org.crt"),
+                    new File("/etc/pki/fedmsg/jenkins-jenkins.cloud.fedoraproject.org.key"));
+                fedmsg.send(signed);
+            } catch (Exception e) {
+                return false;
+            }
+        }
         return true;
     }
 
-    // Overridden for better type safety.
-    // If your plugin doesn't really define any property on Descriptor,
-    // you don't have to do this.
+    @Override
+    public BuildStepMonitor getRequiredMonitorService() {
+        return BuildStepMonitor.BUILD;
+    }
+
     @Override
     public DescriptorImpl getDescriptor() {
         return (DescriptorImpl)super.getDescriptor();
     }
 
-    /**
-     * Descriptor for {@link FedmsgEmitter}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
     @Extension // This indicates to Jenkins that this is an implementation of an extension point.
-    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+    public static final class DescriptorImpl extends BuildStepDescriptor<Publisher> {
         /**
          * To persist global configuration information,
          * simply store it in a field and call save().
@@ -90,32 +95,14 @@ public class FedmsgEmitter extends Builder {
         private boolean useStaging;
 
         /**
-         * In order to load the persisted global configuration, you have to 
+         * In order to load the persisted global configuration, you have to
          * call load() in the constructor.
          */
         public DescriptorImpl() {
             load();
         }
 
-        /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         */
-        public FormValidation doCheckName(@QueryParameter String value)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
-            return FormValidation.ok();
-        }
-
         public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
             return true;
         }
 
@@ -128,20 +115,13 @@ public class FedmsgEmitter extends Builder {
 
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
-            // To persist global configuration information,
-            // set that to properties and call save().
             useStaging = formData.getBoolean("useStaging");
-            // ^Can also use req.bindJSON(this, formData);
-            //  (easier when there are many fields; need set* methods for this, like setUseStaging)
             save();
-            return super.configure(req,formData);
+            return super.configure(req, formData);
         }
 
         /**
          * This method returns true if the global configuration says we should use staging.
-         *
-         * The method name is bit awkward because global.jelly calls this method to determine
-         * the initial state of the checkbox by the naming convention.
          */
         public boolean getUseStaging() {
             return useStaging;
